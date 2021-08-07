7ea8827515871beea77a823d6ab0ab01dd1bd64f
Morgan DURAND <mdurand@wps.webhelp.com>
2021-08-07 14:20:36 +0200

    forgotten commit

---
+++ drawing/LineRoad.gd
@@ -0,0 +1,63 @@
+tool
+extends Node2D
+
+const POINT_PICK_DISTANCE = 10.0
+var point_picked = false
+var selected_point = null
+var select_index=0
+
+export(int, 0, 100) var offset = 90
+export(int, 0, 100) var steps = 100
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		
+		if point_picked:
+			if selected_point != null and select_index != null:
+				$Line2D.remove_point(select_index)
+				$Line2D.add_point(event.position, select_index)
+				draw_points()
+		else:
+			selected_point = null
+			for point in $Line2D.points:
+				if event.position.distance_to(point) < POINT_PICK_DISTANCE:
+					selected_point = point
+	if event is InputEventMouseButton:
+		if event.pressed:
+			point_picked = true
+			selected_point = null
+			var i=0
+			for point in $Line2D.points:
+				if event.position.distance_to(point) < POINT_PICK_DISTANCE:
+					selected_point = point
+					select_index=i
+				i=i+1
+			print("selected point")
+			print(selected_point)
+		else:
+			point_picked = false
+			selected_point = null
+
+
+func _draw():
+	draw_points()
+
+	if selected_point != null:  # draw selected point
+		draw_circle(selected_point.position, 20, Color(1, 1, 1, 0.3))
+		pass
+
+func draw_points():
+	for point in $Line2D.points:
+		print("points :")
+		print(point)
+		draw_circle(point, 7, Color(1.0, 0.5, 0.5))
